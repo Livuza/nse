c0d58e3d9e1ec962759bff746cb1e595719df4bb
Charles Livuza <livuzacharles@gmail.com>
2022-06-10 22:39:53 +0300

    nse working

--- nse.py
+++ nse.py
@@ -25,7 +25,7 @@ df = df[df["ticker"] == add_selectbox]
 
 add_start_date = st.sidebar.date_input("Start Date")
 add_end_date = st.sidebar.date_input("End Date")
-df = df[df["date"] == add_start_date]
+#df = df[df["date"] == add_start_date]
 #df = df[df["date"] == add_end_date]
 
 #with st.sidebar:
